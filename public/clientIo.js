74760faa838082dc32d27c136b11a2f77867a5e7
Ranjan <mynamerkt3@gmail.com>
2021-09-08 21:03:33 +0530

    bugfix

--- public/clientIo.js
+++ public/clientIo.js
@@ -27,7 +27,7 @@ socket.on("generateGrid",function(roomid,username){
   console.log("here1");
   document.getElementById("game-container").style.display = "flex";
   document.getElementById("userDetail").style.display = "none";
-  document.getElementById("header").display="none";
+  document.getElementById("header").style.display="none";
   document.getElementById("logo").innerHTML += roomid;
 
 });
